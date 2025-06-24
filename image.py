59c3adacbcb2d958457fcb5bf7d8cc9daf89fc87
Arun Verma <arunverma18620@gmail.com>
2025-06-24 10:48:42 +0530

    Update image.py

--- image.py
+++ image.py
@@ -11,7 +11,7 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 #Display the grayscale image
 cv2.imshow('Black and White Image', gray)
-cv2.waitKey(0)              # We use this for Pause and wait until the user presses a key otherwise window open and immediately close.
+cv2.waitKey(0)              # We use this for Pause and wait until the user presses a key, if don't use this window open and immediately close.
 cv2.destroyAllWindows()     # We use this for close all the image windows that were opened. after the user presses a key, it will clean up and close everything.
 
 #Optionally, save the grayscale image
